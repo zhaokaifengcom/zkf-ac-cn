82ead1e6720f5713c8555f0958259d7c153bc36e
zhaokaifeng <zkfdyx@outlook.com>
2024-03-29 21:32:43 +0800

    XiaoYan Update

---
+++ 01-push.bat
@@ -0,0 +1,3 @@
+git add .
+git commit -m "XiaoYan Update"
+git push
---
+++ pelicanconf.py
@@ -0,0 +1,31 @@
+AUTHOR = '赵凯峰'
+SITENAME = '赵凯峰的个人网站'
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'Asia/Shanghai'
+
+DEFAULT_LANG = 'Chinese (Simplified)'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("考研数学", "https://zhaokaifeng.com/"),
+)
+
+# Social widget
+SOCIAL = (
+    ("个人简介", "https://zkf.ac.cn/guan-yu.html"),
+)
+
+DEFAULT_PAGINATION = 16
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
